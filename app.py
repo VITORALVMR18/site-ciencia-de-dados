13d25e9666c6a219af1547168ae648d94a22e853
VITORALVMR18 <vitor1810176@gmail.com>
2024-11-18 10:19:33 -0300

    .

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, 
-            template_folder ="siteciencia/templates",)
+            template_folder ="siteciencia/templates", static_folder="siteciencia/static")
 
 @app.route("/")
 def index():
